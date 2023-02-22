934c8826e1fe62120fbc72c992ca696b683ff7a9
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2023-02-21 20:58:58 -0600

    refactor: change stack and queue structs

--- EDD_Proyecto1_Fase1/controller/Queue.go
+++ EDD_Proyecto1_Fase1/controller/Queue.go
@@ -1,5 +1,42 @@
 package controller
 
 type Queue struct {
-	items *SinglyLinkedList
+	Head, Tail *Node
+	Size       int
+}
+
+func (q *Queue) Enqueue(data interface{}) {
+	node := &Node{Data: data}
+	if q.Size == 0 {
+		q.Head = node
+		q.Tail = node
+	} else {
+		q.Tail.Next = node
+		q.Tail = node
+	}
+}
+
+func (q *Queue) Dequeue() interface{} {
+	if q.Size == 0 {
+		return nil
+	}
+	data := q.Head.Data
+	q.Head = q.Head.Next
+	q.Size--
+	return data
+}
+
+func (q *Queue) IsEmpty() bool {
+	return q.Size == 0
+}
+
+func (q *Queue) SizeQueue() int {
+	return q.Size
+}
+
+func (q *Queue) Peek() interface{} {
+	if q.Size == 0 {
+		return nil
+	}
+	return q.Head.Data
 }
--- EDD_Proyecto1_Fase1/controller/Stack.go
+++ EDD_Proyecto1_Fase1/controller/Stack.go
@@ -1,5 +1,44 @@
 package controller
 
 type Stack struct {
-	items *SinglyLinkedList
+	Top  *SimpleNode
+	Size int
+}
+
+func (s *Stack) IsEmpty() bool {
+	return s.Size == 0
+}
+
+func (s *Stack) Push(data interface{}) *SimpleNode {
+	node := NewSimpleNode(data)
+
+	if s.IsEmpty() {
+		s.Top = node
+	} else {
+		node.Next = s.Top
+		s.Top = node
+	}
+	s.Size++
+	return node
+}
+
+func (s *Stack) Pop() interface{} {
+	if s.IsEmpty() {
+		return nil
+	}
+	data := s.Top.Data
+	s.Top = s.Top.Next
+	s.Size--
+	return data
+}
+
+func (s *Stack) Peek() interface{} {
+	if s.IsEmpty() {
+		return nil
+	}
+	return s.Top.Data
+}
+
+func (s *Stack) SizeStack() int {
+	return s.Size
 }
